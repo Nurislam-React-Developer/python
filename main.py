7510d85eedbf6c66946a794a2ed65353afe5add9
Nurislam-React-Developer <98372q@gmailc.com>
2026-01-16 23:11:52 +0600

    python

---
+++ main.py
@@ -0,0 +1,96 @@
+# name = "Nurs"
+
+# age = 20
+
+# print(name)
+
+# print(age)
+# print(5 + 7)
+
+
+# name = input("What is your name? ")
+# age = int(input("How old are you?"))
+
+# print("Hello " + name)
+# print("Через год тебе будет", age + 1)
+
+# if age >= 18:
+#     print("Ты взрослый")
+# else :
+#     print("Ты еще ребенок")
+
+# if name == "Nurs":
+#     print("Привет, создатель программы!")
+# else :
+#     print("Привет, " + name)
+
+# print("Hello " + name)
+
+#! 1task
+# print("Hello, Python", 10)
+
+#? 2task
+
+# a = 7
+# b = 3
+
+# print("Сумма:", a + b)
+# print("Разность:" , a - b)
+# print("Произведение:", a * b)
+
+#! 3task
+
+# name = input("Как тебя зовут?")
+# age = int(input("Сколько тебе лет?"))
+
+# print("Привет, " + name)
+# print("Тебе: ", age , "лет")
+
+#! 4task
+
+# sum1 = int(input("Введите сумму: "))
+# sum2 = int(input("Введите сумму: "))
+# total = sum1 + sum2
+# average = total / 2
+# print("Общая сумма:", total)
+# print("Среднее значение:", average)
+
+
+# num = int(input("Введите число: "))
+# print("Ваше число:", num)
+
+#? 5task
+
+# name = input("What is your name?")
+# print(f"Имя пользователя: {name}")
+
+#? 6task
+
+# birth_year = int(input("When were you born?"))
+# print(f"Ваш возраст: {2026 - birth_year}")
+
+
+#! 10task
+
+# number = int(input("Введите число: "))
+
+# if number > 0:
+#     print("Положительное число")
+# elif number < 0:
+#  print("Отрицательное число")
+# else:
+#     print("Ноль")
+
+#! 11task
+
+# age = int(input("Введите ваш возраст: "))
+
+# if age < 18:
+#     print("Вы несовершеннолетний")
+# else:
+#     print("Вы совершеннолетний")
+
+
+#! 12task
+
+number = int(input("Введите число: "))
